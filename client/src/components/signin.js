d720f1af0d1d70dc36338fca2d8c72b813d2da4b
Gadupathi saikiran <120211978+gadupathisaikiran@users.noreply.github.com>
2023-02-06 02:15:53 +0530

    Update signin.js

--- client/src/components/signin.js
+++ client/src/components/signin.js
@@ -17,7 +17,7 @@ const [Userdata,setUserdata]=useState()
 async function signin(userdata)
 {
     try{
-        const res=await axios.post("https://todoappp-backend.onrender.com/user/signin",userdata)
+        const res=await axios.post("https://todo-backend-g8sg.onrender.com/user/signin",userdata)
 
        
         console.log(res)
