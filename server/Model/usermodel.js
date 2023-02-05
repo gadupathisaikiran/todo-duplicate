64c9e1eaee3e5e9d3aecca1ea3c125aee02425a2
Gadupathi saikiran <120211978+gadupathisaikiran@users.noreply.github.com>
2023-02-06 02:00:54 +0530

    Update usermodel.js

--- server/Model/usermodel.js
+++ server/Model/usermodel.js
@@ -6,6 +6,8 @@ const userschema=mongoose.Schema
 const Userschema=new userschema({
 
     email:{type:String,unique:true},
+    valid:{type:Boolean},
+    verifynum:{type:Number},
     password:{type:String}
 
 })
